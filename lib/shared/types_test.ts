e6d6e192f314c24476cfdc43a3400219635f0a8a
Taymon A. Beal <taymon@google.com>
2021-05-11 07:13:12 -0400

    Tweak definition of isKeyValueObject to handle other types (#38)
    This isn't needed for JSON but may be needed for other stuff later and is slightly more correct regardless.

--- lib/shared/types_test.ts
+++ lib/shared/types_test.ts
@@ -22,6 +22,12 @@ describe("isObject", () => {
   it("should return true for []", () => {
     expect(isObject([])).toBeTrue();
   });
+  it("should return true for function", () => {
+    expect(isObject(() => null)).toBeTrue();
+  });
+  it("should return true for Date", () => {
+    expect(isObject(new Date())).toBeTrue();
+  });
   it("should return false for null", () => {
     expect(isObject(null)).toBeFalse();
   });
@@ -36,6 +42,12 @@ describe("isKeyValueObject", () => {
   });
   it("should return false for []", () => {
     expect(isKeyValueObject([])).toBeFalse();
+  });
+  it("should return false for function", () => {
+    expect(isKeyValueObject(() => null)).toBeFalse();
+  });
+  it("should return false for Date", () => {
+    expect(isKeyValueObject(new Date())).toBeFalse();
   });
   it("should return false for null", () => {
     expect(isKeyValueObject(null)).toBeFalse();
