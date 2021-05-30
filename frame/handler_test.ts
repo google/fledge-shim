google/fledge-shim

commit 7d2c9162ed2c0986d8782362b5d2637d2d7488fa

Make database operations return boolean success (#108)
This is the first step of migrating the frame code (but not the library code) away from exceptions for circumstances that aren't bugs in the code (in particular, IndexedDB I/O errors). This will make the code more robust (since it's generally safe to assume that things won't throw) and facilitate error telemetry when we get to that point.

frame/handler.ts
@@ -38,9 +38,13 @@ export async function handleRequest(
     }
     switch (request.kind) {
       case RequestKind.JOIN_AD_INTEREST_GROUP:
+        // Ignore return value; any errors will have already been logged and
+        // there's nothing more to be done about them.
         await storeInterestGroup(request.group);
         return;
       case RequestKind.LEAVE_AD_INTEREST_GROUP:
+        // Ignore return value; any errors will have already been logged and
+        // there's nothing more to be done about them.
         await deleteInterestGroup(request.group.name);
         return;
       case RequestKind.RUN_AD_AUCTION: {
@@ -50,7 +54,17 @@ export async function handleRequest(
           );
         }
         const [port] = ports;
-        const token = await runAdAuction(request.config, hostname);
+        const result = await runAdAuction(request.config, hostname);
+        let token;
+        switch (result) {
+          case true:
+            token = null;
+            break;
+          case false:
+            throw new Error("IndexedDB error");
+          default:
+            token = result;
+        }
         const response: RunAdAuctionResponse = [true, token];
         port.postMessage(response);
         port.close();

frame/handler_test.ts
@@ -87,7 +87,7 @@ describe("handleRequest", () => {
   it("should join an interest group", async () => {
     await handleRequest(joinMessageEvent, hostname);
     const callback = jasmine.createSpy<InterestGroupCallback>();
-    await forEachInterestGroup(callback);
+    expect(await forEachInterestGroup(callback)).toBeTrue();
     expect(callback).toHaveBeenCalledOnceWith(group);
   });
 
@@ -103,7 +103,7 @@ describe("handleRequest", () => {
       hostname
     );
     const callback = jasmine.createSpy<InterestGroupCallback>();
-    await forEachInterestGroup(callback);
+    expect(await forEachInterestGroup(callback)).toBeTrue();
     expect(callback).toHaveBeenCalledOnceWith(group);
   });
 
@@ -119,7 +119,7 @@ describe("handleRequest", () => {
       hostname
     );
     const callback = jasmine.createSpy<InterestGroupCallback>();
-    await forEachInterestGroup(callback);
+    expect(await forEachInterestGroup(callback)).toBeTrue();
     expect(callback).not.toHaveBeenCalled();
   });
 

frame/auction.ts
@@ -16,7 +16,7 @@ import { FetchJsonStatus, tryFetchJson } from "./fetch";
  * Selects the currently stored ad with the highest price, mints a token that
  * can later be used to display that ad (by storing the mapping in
  * `sessionStorage`), and returns the token. If no ads are available, returns
- * `null`.
+ * true. If an error occurs, returns false.
  *
  * Also makes a request to `trustedScoringSignalsUrl`, if one is provided, and
  * validates that it is a JSON object. For now, the data is simply thrown away
@@ -39,19 +39,23 @@ export async function runAdAuction(
   // This is temporary until trustedBiddingSignalsUrl is added.
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   hostname: string
-): Promise<string | null> {
+): Promise<string | boolean> {
   let winner: Ad | undefined;
   const renderingUrls = new Set<string>();
-  await forEachInterestGroup(({ ads }) => {
-    for (const ad of ads) {
-      renderingUrls.add(ad.renderingUrl);
-      if (!winner || ad.metadata.price > winner.metadata.price) {
-        winner = ad;
+  if (
+    !(await forEachInterestGroup(({ ads }) => {
+      for (const ad of ads) {
+        renderingUrls.add(ad.renderingUrl);
+        if (!winner || ad.metadata.price > winner.metadata.price) {
+          winner = ad;
+        }
       }
-    }
-  });
+    }))
+  ) {
+    return false;
+  }
   if (!winner) {
-    return null;
+    return true;
   }
   if (trustedScoringSignalsUrl !== undefined) {
     await fetchAndValidateTrustedSignals(

frame/auction_test.ts
@@ -26,28 +26,32 @@ describe("runAdAuction", () => {
   const hostname = "www.example.com";
 
   it("should return the higher-priced ad from a single interest group", async () => {
-    await storeInterestGroup({ name, ads: [ad1, ad2] });
+    expect(await storeInterestGroup({ name, ads: [ad1, ad2] })).toBeTrue();
     const token = await runAdAuction({}, hostname);
     assertToBeString(token);
     expect(sessionStorage.getItem(token)).toBe(ad2.renderingUrl);
   });
 
   it("should return the higher-priced ad across multiple interest groups", async () => {
-    await storeInterestGroup({ name: "interest group name 1", ads: [ad1] });
-    await storeInterestGroup({ name: "interest group name 2", ads: [ad2] });
+    expect(
+      await storeInterestGroup({ name: "interest group name 1", ads: [ad1] })
+    ).toBeTrue();
+    expect(
+      await storeInterestGroup({ name: "interest group name 2", ads: [ad2] })
+    ).toBeTrue();
     const token = await runAdAuction({}, hostname);
     assertToBeString(token);
     expect(sessionStorage.getItem(token)).toBe(ad2.renderingUrl);
   });
 
-  it("should return null if there are no ads", async () => {
+  it("should return true if there are no ads", async () => {
     const result = await runAdAuction({}, hostname);
-    expect(result).toBeNull();
+    expect(result).toBeTrue();
     expect(sessionStorage.length).toBe(0);
   });
 
   it("should return tokens in the expected format", async () => {
-    await storeInterestGroup({ name, ads: [ad1] });
+    expect(await storeInterestGroup({ name, ads: [ad1] })).toBeTrue();
     for (let i = 0; i < 100; i++) {
       expect(await runAdAuction({}, hostname)).toMatch(/^[0-9a-f]{32}$/);
     }
@@ -64,12 +68,19 @@ describe("runAdAuction", () => {
   };
 
   it("should fetch trusted scoring signals for ads in a single interest group", async () => {
-    await storeInterestGroup({ name, ads: [ad1, ad2] });
+    expect(
+      await storeInterestGroup({
+        name,
+        ads: [ad1, ad2],
+      })
+    ).toBeTrue();
     const fakeServerHandler = jasmine
       .createSpy<FakeServerHandler>()
       .and.resolveTo(trustedSignalsResponse);
     setFakeServerHandler(fakeServerHandler);
-    await runAdAuction({ trustedScoringSignalsUrl }, hostname);
+    expect(
+      await runAdAuction({ trustedScoringSignalsUrl }, hostname)
+    ).toBeTruthy();
     expect(fakeServerHandler).toHaveBeenCalledOnceWith(
       jasmine.objectContaining<FakeRequest>({
         url: new URL(
@@ -84,15 +95,34 @@ describe("runAdAuction", () => {
   });
 
   it("should fetch trusted scoring signals for ads across multiple interest groups", async () => {
-    await storeInterestGroup({ name: "interest group 1", ads: [ad1] });
-    await storeInterestGroup({ name: "interest group 2", ads: [ad2, ad3] });
-    await storeInterestGroup({ name: "interest group 3", ads: [ad4] });
-    await storeInterestGroup({ name: "interest group 4", ads: [] });
+    expect(
+      await storeInterestGroup({
+        name: "interest group 1",
+        ads: [ad1],
+      })
+    ).toBeTrue();
+    expect(
+      await storeInterestGroup({
+        name: "interest group 2",
+        ads: [ad2, ad3],
+      })
+    ).toBeTrue();
+    expect(
+      await storeInterestGroup({ name: "interest group 3", ads: [ad4] })
+    ).toBeTrue();
+    expect(
+      await storeInterestGroup({
+        name: "interest group 4",
+        ads: [],
+      })
+    ).toBeTrue();
     const fakeServerHandler = jasmine
       .createSpy<FakeServerHandler>()
       .and.resolveTo(trustedSignalsResponse);
     setFakeServerHandler(fakeServerHandler);
-    await runAdAuction({ trustedScoringSignalsUrl }, hostname);
+    expect(
+      await runAdAuction({ trustedScoringSignalsUrl }, hostname)
+    ).toBeTruthy();
     expect(fakeServerHandler).toHaveBeenCalledOnceWith(
       jasmine.objectContaining<FakeRequest>({
         url: new URL(
@@ -110,15 +140,22 @@ describe("runAdAuction", () => {
   it("should not fetch trusted scoring signals if there are no ads", async () => {
     const fakeServerHandler = jasmine.createSpy<FakeServerHandler>();
     setFakeServerHandler(fakeServerHandler);
-    await runAdAuction({ trustedScoringSignalsUrl }, hostname);
+    expect(
+      await runAdAuction({ trustedScoringSignalsUrl }, hostname)
+    ).toBeTruthy();
     expect(fakeServerHandler).not.toHaveBeenCalled();
   });
 
   it("should not fetch trusted scoring signals if no URL is provided", async () => {
-    await storeInterestGroup({ name, ads: [ad1, ad2] });
+    expect(
+      await storeInterestGroup({
+        name,
+        ads: [ad1, ad2],
+      })
+    ).toBeTrue();
     const fakeServerHandler = jasmine.createSpy<FakeServerHandler>();
     setFakeServerHandler(fakeServerHandler);
-    await runAdAuction({}, hostname);
+    expect(await runAdAuction({}, hostname)).toBeTruthy();
     expect(fakeServerHandler).not.toHaveBeenCalled();
   });
 
@@ -128,7 +165,9 @@ describe("runAdAuction", () => {
     setFakeServerHandler(fakeServerHandler);
     const consoleSpy = spyOnAllFunctions(console);
     const notUrl = "This string is not a URL.";
-    await runAdAuction({ trustedScoringSignalsUrl: notUrl }, hostname);
+    expect(
+      await runAdAuction({ trustedScoringSignalsUrl: notUrl }, hostname)
+    ).toBeTruthy();
     expect(fakeServerHandler).not.toHaveBeenCalled();
     expect(consoleSpy.warn).toHaveBeenCalledOnceWith(
       jasmine.any(String),
@@ -142,13 +181,15 @@ describe("runAdAuction", () => {
     setFakeServerHandler(fakeServerHandler);
     const consoleSpy = spyOnAllFunctions(console);
     const url = trustedScoringSignalsUrl + "?key=value";
-    await runAdAuction({ trustedScoringSignalsUrl: url }, hostname);
+    expect(
+      await runAdAuction({ trustedScoringSignalsUrl: url }, hostname)
+    ).toBeTruthy();
     expect(fakeServerHandler).not.toHaveBeenCalled();
     expect(consoleSpy.warn).toHaveBeenCalledOnceWith(jasmine.any(String), url);
   });
 
   it("should log a warning if MIME type is wrong", async () => {
-    await storeInterestGroup({ name, ads: [ad1, ad2] });
+    expect(await storeInterestGroup({ name, ads: [ad1, ad2] })).toBeTrue();
     const mimeType = "text/html";
     setFakeServerHandler(() =>
       Promise.resolve({
@@ -160,7 +201,9 @@ describe("runAdAuction", () => {
       })
     );
     const consoleSpy = spyOnAllFunctions(console);
-    await runAdAuction({ trustedScoringSignalsUrl }, hostname);
+    expect(
+      await runAdAuction({ trustedScoringSignalsUrl }, hostname)
+    ).toBeTruthy();
     expect(consoleSpy.warn).toHaveBeenCalledOnceWith(
       jasmine.any(String),
       trustedScoringSignalsUrl + "?keys=about%3Ablank%231,about%3Ablank%232",
@@ -170,10 +213,12 @@ describe("runAdAuction", () => {
   });
 
   it("should log a warning if JSON is ill-formed", async () => {
-    await storeInterestGroup({ name, ads: [ad1, ad2] });
+    expect(await storeInterestGroup({ name, ads: [ad1, ad2] })).toBeTrue();
     setFakeServerHandler(() => Promise.resolve({ headers, body: '{"a": 1?}' }));
     const consoleSpy = spyOnAllFunctions(console);
-    await runAdAuction({ trustedScoringSignalsUrl }, hostname);
+    expect(
+      await runAdAuction({ trustedScoringSignalsUrl }, hostname)
+    ).toBeTruthy();
     expect(consoleSpy.warn).toHaveBeenCalledOnceWith(
       jasmine.any(String),
       trustedScoringSignalsUrl + "?keys=about%3Ablank%231,about%3Ablank%232",
@@ -183,7 +228,7 @@ describe("runAdAuction", () => {
   });
 
   it("should log a warning if JSON value is not an object", async () => {
-    await storeInterestGroup({ name, ads: [ad1, ad2] });
+    expect(await storeInterestGroup({ name, ads: [ad1, ad2] })).toBeTrue();
     setFakeServerHandler(() =>
       Promise.resolve({
         headers,
@@ -191,7 +236,9 @@ describe("runAdAuction", () => {
       })
     );
     const consoleSpy = spyOnAllFunctions(console);
-    await runAdAuction({ trustedScoringSignalsUrl }, hostname);
+    expect(
+      await runAdAuction({ trustedScoringSignalsUrl }, hostname)
+    ).toBeTruthy();
     expect(consoleSpy.warn).toHaveBeenCalledOnceWith(
       jasmine.any(String),
       trustedScoringSignalsUrl + "?keys=about%3Ablank%231,about%3Ablank%232",
@@ -201,36 +248,42 @@ describe("runAdAuction", () => {
   });
 
   it("should not log on network error", async () => {
-    await storeInterestGroup({ name, ads: [ad1, ad2] });
+    expect(await storeInterestGroup({ name, ads: [ad1, ad2] })).toBeTrue();
     const consoleSpy = spyOnAllFunctions(console);
-    await runAdAuction(
-      { trustedScoringSignalsUrl: "invalid-scheme://" },
-      hostname
-    );
+    expect(
+      await runAdAuction(
+        { trustedScoringSignalsUrl: "invalid-scheme://" },
+        hostname
+      )
+    ).toBeTruthy();
     expect(consoleSpy.error).not.toHaveBeenCalled();
     expect(consoleSpy.warn).not.toHaveBeenCalled();
   });
 
-  it("should not log if trusted scoring signals are fetched successfully", async () => {
-    await storeInterestGroup({ name, ads: [ad1, ad2] });
+  it("should not log if trusted bidding and scoring signals are fetched successfully", async () => {
+    expect(await storeInterestGroup({ name, ads: [ad1, ad2] })).toBeTrue();
     setFakeServerHandler(() => Promise.resolve(trustedSignalsResponse));
     const consoleSpy = spyOnAllFunctions(console);
-    await runAdAuction({ trustedScoringSignalsUrl }, hostname);
+    expect(
+      await runAdAuction({ trustedScoringSignalsUrl }, hostname)
+    ).toBeTruthy();
     expect(consoleSpy.error).not.toHaveBeenCalled();
     expect(consoleSpy.warn).not.toHaveBeenCalled();
   });
 
   it("should not log if there are no ads", async () => {
     const consoleSpy = spyOnAllFunctions(console);
-    await runAdAuction({ trustedScoringSignalsUrl }, hostname);
+    expect(
+      await runAdAuction({ trustedScoringSignalsUrl }, hostname)
+    ).toBeTruthy();
     expect(consoleSpy.error).not.toHaveBeenCalled();
     expect(consoleSpy.warn).not.toHaveBeenCalled();
   });
 
   it("should not log if no URL is provided", async () => {
-    await storeInterestGroup({ name, ads: [ad1, ad2] });
+    expect(await storeInterestGroup({ name, ads: [ad1, ad2] })).toBeTrue();
     const consoleSpy = spyOnAllFunctions(console);
-    await runAdAuction({}, hostname);
+    expect(await runAdAuction({}, hostname)).toBeTruthy();
     expect(consoleSpy.error).not.toHaveBeenCalled();
     expect(consoleSpy.warn).not.toHaveBeenCalled();
   });

frame/db_schema.ts
@@ -55,14 +55,14 @@ function recordFromInterestGroup(group: CanonicalInterestGroup) {
 }
 
 /**
- * Stores an interest group in IndexedDB. If there's already one with the same
- * name, each property value of the existing interest group is overwritten if
- * the new interest group has a defined value for that property, but left
- * unchanged if it does not. Note that there is no way to delete a property of
- * an existing interest group without overwriting it with a defined value or
- * deleting the whole interest group.
+ * Stores an interest group in IndexedDB and returns whether it was committed
+ * successfully. If there's already one with the same name, each property value
+ * of the existing interest group is overwritten if the new interest group has a
+ * defined value for that property, but left unchanged if it does not. Note that
+ * there is no way to delete a property of an existing interest group without
+ * overwriting it with a defined value or deleting the whole interest group.
  */
-export function storeInterestGroup(group: InterestGroup): Promise<void> {
+export function storeInterestGroup(group: InterestGroup): Promise<boolean> {
   return useStore("readwrite", (store) => {
     const { name } = group;
     const cursorRequest = store.openCursor(name);
@@ -98,10 +98,10 @@ export function storeInterestGroup(group: InterestGroup): Promise<void> {
 }
 
 /**
- * Deletes an interest group from IndexedDB. If there isn't one with the given
- * name, does nothing.
+ * Deletes an interest group from IndexedDB and returns whether it was committed
+ * successfully. If there isn't one with the given name, does nothing.
  */
-export function deleteInterestGroup(name: string): Promise<void> {
+export function deleteInterestGroup(name: string): Promise<boolean> {
   return useStore("readwrite", (store) => {
     store.delete(name);
   });
@@ -110,10 +110,13 @@ export function deleteInterestGroup(name: string): Promise<void> {
 /** Iteration callback type for `forEachInterestGroup`. */
 export type InterestGroupCallback = (group: CanonicalInterestGroup) => void;
 
-/** Iterates over all interest groups currently stored in IndexedDB. */
+/**
+ * Iterates over all interest groups currently stored in IndexedDB and returns
+ * whether they were all read successfully.
+ */
 export function forEachInterestGroup(
   callback: InterestGroupCallback
-): Promise<void> {
+): Promise<boolean> {
   return useStore("readonly", (store) => {
     const cursorRequest = store.openCursor();
     cursorRequest.onsuccess = () => {

frame/db_schema_test.ts
@@ -25,26 +25,28 @@ describe("db_schema:", () => {
         name,
         ads: [{ renderingUrl: "about:blank", metadata: { price: 0.02 } }],
       };
-      await storeInterestGroup(group);
+      expect(await storeInterestGroup(group)).toBeTrue();
       const callback = jasmine.createSpy<InterestGroupCallback>();
-      await forEachInterestGroup(callback);
+      expect(await forEachInterestGroup(callback)).toBeTrue();
       expect(callback).toHaveBeenCalledOnceWith(group);
     });
 
     it("should read multiple interest groups from IndexedDB", async () => {
-      await useStore("readwrite", (store) => {
-        store.add([["about:blank#1", 0.01]], "interest group name 1");
-        store.add([], "interest group name 2");
-        store.add(
-          [
-            ["about:blank#2", 0.02],
-            ["about:blank#3", 0.03],
-          ],
-          "interest group name 3"
-        );
-      });
+      expect(
+        await useStore("readwrite", (store) => {
+          store.add([["about:blank#1", 0.01]], "interest group name 1");
+          store.add([], "interest group name 2");
+          store.add(
+            [
+              ["about:blank#2", 0.02],
+              ["about:blank#3", 0.03],
+            ],
+            "interest group name 3"
+          );
+        })
+      ).toBeTrue();
       const callback = jasmine.createSpy<InterestGroupCallback>();
-      await forEachInterestGroup(callback);
+      expect(await forEachInterestGroup(callback)).toBeTrue();
       expect(callback).toHaveBeenCalledTimes(3);
       expect(callback).toHaveBeenCalledWith({
         name: "interest group name 1",
@@ -70,16 +72,16 @@ describe("db_schema:", () => {
         name,
         ads: [{ renderingUrl: "about:blank", metadata: { price: 0.02 } }],
       };
-      await storeInterestGroup(group);
+      expect(await storeInterestGroup(group)).toBeTrue();
       const callback = jasmine.createSpy<InterestGroupCallback>();
-      await forEachInterestGroup(callback);
+      expect(await forEachInterestGroup(callback)).toBeTrue();
       expect(callback).toHaveBeenCalledOnceWith(group);
     });
 
     it("should write an ad without all fields present", async () => {
-      await storeInterestGroup({ name });
+      expect(await storeInterestGroup({ name })).toBeTrue();
       const callback = jasmine.createSpy<InterestGroupCallback>();
-      await forEachInterestGroup(callback);
+      expect(await forEachInterestGroup(callback)).toBeTrue();
       expect(callback).toHaveBeenCalledOnceWith({
         name,
         ads: [],
@@ -87,47 +89,55 @@ describe("db_schema:", () => {
     });
 
     it("should overwrite an existing ad", async () => {
-      await storeInterestGroup({
-        name,
-        ads: [{ renderingUrl: "about:blank#1", metadata: { price: 0.01 } }],
-      });
-      await storeInterestGroup({
-        name,
-        ads: [{ renderingUrl: "about:blank#2", metadata: { price: 0.02 } }],
-      });
+      expect(
+        await storeInterestGroup({
+          name,
+          ads: [{ renderingUrl: "about:blank#1", metadata: { price: 0.01 } }],
+        })
+      ).toBeTrue();
+      expect(
+        await storeInterestGroup({
+          name,
+          ads: [{ renderingUrl: "about:blank#2", metadata: { price: 0.02 } }],
+        })
+      ).toBeTrue();
       const callback = jasmine.createSpy<InterestGroupCallback>();
-      await forEachInterestGroup(callback);
+      expect(await forEachInterestGroup(callback)).toBeTrue();
       expect(callback).toHaveBeenCalledOnceWith({
         name,
         ads: [{ renderingUrl: "about:blank#2", metadata: { price: 0.02 } }],
       });
     });
+  });
 
-    describe("deleteInterestGroup", () => {
-      it("should delete an interest group", async () => {
+  describe("deleteInterestGroup", () => {
+    it("should delete an interest group", async () => {
+      expect(
         await storeInterestGroup({
           name: "interest group name 1",
           ads: [{ renderingUrl: "about:blank#1", metadata: { price: 0.01 } }],
-        });
+        })
+      ).toBeTrue();
+      expect(
         await storeInterestGroup({
           name: "interest group name 2",
           ads: [{ renderingUrl: "about:blank#2", metadata: { price: 0.02 } }],
-        });
-        await deleteInterestGroup("interest group name 2");
-        const callback = jasmine.createSpy<InterestGroupCallback>();
-        await forEachInterestGroup(callback);
-        expect(callback).toHaveBeenCalledOnceWith({
-          name: "interest group name 1",
-          ads: [{ renderingUrl: "about:blank#1", metadata: { price: 0.01 } }],
-        });
+        })
+      ).toBeTrue();
+      expect(await deleteInterestGroup("interest group name 2")).toBeTrue();
+      const callback = jasmine.createSpy<InterestGroupCallback>();
+      expect(await forEachInterestGroup(callback)).toBeTrue();
+      expect(callback).toHaveBeenCalledOnceWith({
+        name: "interest group name 1",
+        ads: [{ renderingUrl: "about:blank#1", metadata: { price: 0.01 } }],
       });
+    });
 
-      it("should do nothing when deleting a nonexistent interest group", async () => {
-        await deleteInterestGroup("interest group name");
-        const callback = jasmine.createSpy<InterestGroupCallback>();
-        await forEachInterestGroup(callback);
-        expect(callback).not.toHaveBeenCalled();
-      });
+    it("should do nothing when deleting a nonexistent interest group", async () => {
+      expect(await deleteInterestGroup("interest group name")).toBeTrue();
+      const callback = jasmine.createSpy<InterestGroupCallback>();
+      expect(await forEachInterestGroup(callback)).toBeTrue();
+      expect(callback).not.toHaveBeenCalled();
     });
   });
 });

commit 3c4336e963f44ebe59f746623d145cb4dce021c5

Do not throw in the frame except in case of a bug
This completes the work started in #108 and continued in #114.

frame/handler.ts
@@ -15,6 +15,7 @@ import {
   RunAdAuctionResponse,
 } from "../lib/shared/protocol";
 import { runAdAuction } from "./auction";
+import { logError } from "./console";
 import { deleteInterestGroup, storeInterestGroup } from "./db_schema";
 
 /**
@@ -34,7 +35,8 @@ export async function handleRequest(
   try {
     const request = requestFromMessageData(data);
     if (!request) {
-      throw new Error(`Malformed request: ${JSON.stringify(data)}`);
+      logError("Malformed request from parent window:", [data]);
+      return;
     }
     switch (request.kind) {
       case RequestKind.JOIN_AD_INTEREST_GROUP:
@@ -49,18 +51,16 @@ export async function handleRequest(
         return;
       case RequestKind.RUN_AD_AUCTION: {
         if (ports.length !== 1) {
-          throw new Error(
-            `Port transfer mismatch during request: expected 1 port, but received ${ports.length}`
+          logError(
+            `Port transfer mismatch in request from parent window: Expected 1 port, but received ${ports.length}`
           );
+          return;
         }
         const [port] = ports;
         const response: RunAdAuctionResponse = await runAdAuction(
           request.config,
           hostname
         );
-        if (response === false) {
-          throw new Error("IndexedDB error");
-        }
         port.postMessage(response);
         return;
       }

frame/main.ts
@@ -10,6 +10,7 @@
  */
 
 import { VERSION, VERSION_KEY } from "../lib/shared/version";
+import { logError } from "./console";
 import { handleRequest } from "./handler";
 
 /**
@@ -23,7 +24,8 @@ import { handleRequest } from "./handler";
 export function main(win: Window): void {
   const parentOrigin = win.location.ancestorOrigins[0];
   if (parentOrigin === undefined) {
-    throw new Error("Frame can't run as a top-level document");
+    logError("Frame can't run as a top-level document");
+    return;
   }
   const fragment = win.location.hash;
   if (fragment) {
@@ -43,10 +45,10 @@ function connect(targetWindow: Window, targetOrigin: string) {
 }
 
 function render(doc: Document, fragment: string) {
-  const token = fragment.substring(1);
-  const renderUrl = sessionStorage.getItem(token);
+  const renderUrl = sessionStorage.getItem(fragment.substring(1));
   if (!renderUrl) {
-    throw new Error(`Invalid token: ${token}`);
+    logError("Invalid token:", [fragment]);
+    return;
   }
   const iframe = doc.createElement("iframe");
   iframe.src = renderUrl;

frame/handler_test.ts
@@ -21,12 +21,14 @@ import {
   FakeServerHandler,
   setFakeServerHandler,
 } from "../testing/http";
+import { addMessagePortMatchers } from "../testing/messaging";
 import { clearStorageBeforeAndAfter } from "../testing/storage";
 import { forEachInterestGroup, InterestGroupCallback } from "./db_schema";
 import { handleRequest } from "./handler";
 
 describe("handleRequest", () => {
   clearStorageBeforeAndAfter();
+  beforeAll(addMessagePortMatchers);
 
   const hostname = "www.example.com";
 
@@ -39,20 +41,49 @@ describe("handleRequest", () => {
     [RequestKind.LEAVE_AD_INTEREST_GROUP, 0.02],
     [RequestKind.RUN_AD_AUCTION, []],
   ]) {
-    it(`should throw and not reply to ${JSON.stringify(
+    it(`should log an error, close ports, and not reply to ${JSON.stringify(
       badInput
     )}`, async () => {
+      const consoleSpy = spyOnAllFunctions(console);
       const { port1: receiver, port2: sender } = new MessageChannel();
       const messageEventPromise = awaitMessageToPort(receiver);
-      await expectAsync(
-        handleRequest(
-          new MessageEvent("message", { data: badInput, ports: [sender] }),
-          hostname
-        )
-      ).toBeRejectedWithError();
+      await handleRequest(
+        new MessageEvent("message", { data: badInput, ports: [sender] }),
+        hostname
+      );
+      expect(consoleSpy.error).toHaveBeenCalledOnceWith(
+        jasmine.any(String),
+        badInput
+      );
+      await expectAsync(receiver).not.toBeEntangledWith(sender);
       await expectAsync(messageEventPromise).toBePending();
     });
   }
+
+  it("should log an error, close ports, and not reply on port transfer mismatch", async () => {
+    const consoleSpy = spyOnAllFunctions(console);
+    const channel = new MessageChannel();
+    const messageEventPromise = awaitMessageToPort(channel.port1);
+    const otherChannel = new MessageChannel();
+    await handleRequest(
+      new MessageEvent("message", {
+        data: messageDataFromRequest({
+          kind: RequestKind.RUN_AD_AUCTION,
+          config: {},
+        }),
+        ports: [channel.port2, otherChannel.port1, otherChannel.port2],
+      }),
+      hostname
+    );
+    expect(consoleSpy.error).toHaveBeenCalledOnceWith(
+      jasmine.stringMatching(/.*\b3\b.*/)
+    );
+    await expectAsync(channel.port1).not.toBeEntangledWith(channel.port2);
+    await expectAsync(otherChannel.port1).not.toBeEntangledWith(
+      otherChannel.port2
+    );
+    await expectAsync(messageEventPromise).toBePending();
+  });
 
   const name = "interest group name";
   const trustedBiddingSignalsUrl = "https://trusted-server.test/bidding";
@@ -162,5 +193,6 @@ describe("handleRequest", () => {
     );
     expect(consoleSpy.error).not.toHaveBeenCalled();
     expect(consoleSpy.warn).not.toHaveBeenCalled();
+    await expectAsync(receiver).not.toBeEntangledWith(sender);
   });
 });

frame/main_test.ts
@@ -117,20 +117,23 @@ describe("main", () => {
     expect(innerIframe.scrolling).toEqual("no");
   });
 
-  it("should throw on invalid token", () => {
+  it("should log an error on invalid token", () => {
+    const consoleSpy = spyOnAllFunctions(console);
     const iframe = document.createElement("iframe");
     iframe.src = "about:blank#" + token;
     document.body.appendChild(iframe);
     const win = iframe.contentWindow;
     assertToBeTruthy(win);
-    expect(() => {
-      main(win);
-    }).toThrowError();
+    main(win);
+    expect(consoleSpy.error).toHaveBeenCalledOnceWith(
+      jasmine.any(String),
+      "#" + token
+    );
   });
 
-  it("should throw if running on top window", () => {
-    expect(() => {
-      main(top);
-    }).toThrowError();
+  it("should log an error if running on top window", () => {
+    const consoleSpy = spyOnAllFunctions(console);
+    main(top);
+    expect(consoleSpy.error).toHaveBeenCalledOnceWith(jasmine.any(String));
   });
 });